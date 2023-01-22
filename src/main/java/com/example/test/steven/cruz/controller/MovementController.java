68fce131811fe39a4b854c8dc6ead5d880f7b722
stevencruz <steven.cruz.bonito.1988@gmail.com>
2023-01-22 12:32:01 -0500

    se eliminaron import que no se usa

--- src/main/java/com/example/test/steven/cruz/controller/MovementController.java
+++ src/main/java/com/example/test/steven/cruz/controller/MovementController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.test.steven.cruz.dto.ClientRequest;
 import com.example.test.steven.cruz.dto.FinalResponse;
 import com.example.test.steven.cruz.dto.MovementRequest;
 import com.example.test.steven.cruz.dto.MovementResponse;
